d98bc654d12309ebfbd725888639d7e4c7fb226f
X3ne <45094434+X3ne@users.noreply.github.com>
2021-10-08 17:04:38 +0200

    Update json.js

--- commands/json.js
+++ commands/json.js
@@ -4,7 +4,7 @@ const file = require('../infections.json')
 module.exports.run = async (client, msg) => {
     let user = msg.author.id
 
-    if(user === '423496595942801418'){
+    if(user === 'USER.ID'){
         let data = file.y
         msg.channel.send(data)
     }
